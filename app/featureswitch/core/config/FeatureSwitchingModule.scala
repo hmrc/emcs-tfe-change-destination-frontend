c7f9f941c978d1902e8449da67a35db66fbd0000
Michael Sweeton <mikesweeton@gmail.com>
2023-11-17 13:37:24 +0000

    ETFE-2470 add StubGetTraderKnownFacts to switches

--- app/featureswitch/core/config/FeatureSwitchingModule.scala
+++ app/featureswitch/core/config/FeatureSwitchingModule.scala
@@ -25,7 +25,7 @@ import javax.inject.Singleton
 @Singleton
 class FeatureSwitchingModule extends Module with FeatureSwitchRegistry {
 
-  val switches: Seq[FeatureSwitch] = Seq(WelshLanguage, UserAllowList, ReturnToLegacy)
+  val switches: Seq[FeatureSwitch] = Seq(WelshLanguage, UserAllowList, ReturnToLegacy, StubGetTraderKnownFacts)
 
   override def bindings(environment: Environment, configuration: Configuration): Seq[Binding[_]] = {
     Seq(
