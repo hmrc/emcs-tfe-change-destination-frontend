6d0e1f18e4e554517baa0289fe2e772876c8ced1
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2024-09-06 12:42:27 +0100

    [ETFE-4492] Minor tidy up

--- test/forms/sections/journeyType/GiveInformationOtherTransportFormProviderSpec.scala
+++ test/forms/sections/journeyType/GiveInformationOtherTransportFormProviderSpec.scala
@@ -16,8 +16,8 @@
 
 package forms.sections.journeyType
 
+import forms.ALPHANUMERIC_REGEX
 import forms.behaviours.StringFieldBehaviours
-import forms.{ALPHANUMERIC_REGEX, XSS_REGEX}
 import play.api.data.FormError
 
 class GiveInformationOtherTransportFormProviderSpec extends StringFieldBehaviours {
@@ -91,7 +91,5 @@ class GiveInformationOtherTransportFormProviderSpec extends StringFieldBehaviour
         )
       }
     }
-
-
   }
 }
