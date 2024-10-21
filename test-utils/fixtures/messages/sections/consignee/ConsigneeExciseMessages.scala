ca4610f6f5d8ed7343393a2c51d04d0680dc7d1a
MJCallahanPage <16954819+MJCallahanPage@users.noreply.github.com>
2024-10-21 15:05:35 +0100

    [ETFE-4616] Add section to title of all section pages (#104)

--- test-utils/fixtures/messages/sections/consignee/ConsigneeExciseMessages.scala
+++ test-utils/fixtures/messages/sections/consignee/ConsigneeExciseMessages.scala
@@ -16,22 +16,22 @@
 
 package fixtures.messages.sections.consignee
 
-import fixtures.messages.{BaseEnglish, BaseMessages, i18n}
+import fixtures.messages.{BaseEnglish, BaseMessages, SectionMessages, i18n}
 
 object ConsigneeExciseMessages {
   sealed trait ViewMessages extends BaseMessages {
     _: i18n =>
 
     val heading = "What is the consignee’s excise registration number (ERN)?"
-    val title = titleHelper(heading)
+    val title = titleHelper(heading, Some(SectionMessages.English.consigneeSubHeading))
     val hint = "An ERN contains 13 characters, starting with 2 letters that represent the member state of the consignee, such as GBWK123456789. It can be found on your approval letter."
 
     val temporaryRegisteredConsigneeHeading = "What is the Temporary Registered Consignee’s authorisation reference?"
-    val temporaryRegisteredConsigneeTitle: String = titleHelper(temporaryRegisteredConsigneeHeading)
+    val temporaryRegisteredConsigneeTitle: String = titleHelper(temporaryRegisteredConsigneeHeading, Some(SectionMessages.English.consigneeSubHeading))
     val temporaryRegisteredConsigneeHint = "This contains 13 characters, starting with 2 letters that represent the member state of the Temporary Registered Consignee. For example, XI12345678900. This is also known as a Temporary Consignment Authorisation (TCA) number."
 
     val temporaryCertifiedConsigneeHeading = "What is the Temporary Certified Consignee’s authorisation reference?"
-    val temporaryCertifiedConsigneeTitle: String = titleHelper(temporaryCertifiedConsigneeHeading)
+    val temporaryCertifiedConsigneeTitle: String = titleHelper(temporaryCertifiedConsigneeHeading, Some(SectionMessages.English.consigneeSubHeading))
     val temporaryCertifiedConsigneeHint = "This contains 13 characters, starting with 2 letters that represent the member state of the Temporary Certified Consignee. For example, XI12345678900. This is also known as a Temporary Consignment Authorisation (TCA) number."
     val errorSameAsExisting = "Consignee ERN must not be the same as the previous submission of this movement"
   }
